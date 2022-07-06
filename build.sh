15de3edc946191e0714abfc5f08735f890234d7b
OsaeAddo <immanueloaddo@gmail.com>
2022-07-06 15:18:17 +0000

    prepare files for productions

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install --upgrade pip
+poetry install
+
+# python prod_manage.py collectstatic --no-input
+python prod-manage.py makemigrations 
+python prod-manage.py migrate
+python prod-manage.py showmigrations
--- mbv/config/settings/prod-settings.py
+++ mbv/config/settings/prod-settings.py
@@ -1,17 +1,32 @@
 from .base_settings import *
 
+import dj_database_url
+import os
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-pzqirc45gpvlkh#wym^fs@!5yb$0x*8a08cy1%gzylu0xv=@92'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
+DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 ALLOWED_HOSTS = []
 
-
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:    
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+    
+    
+DATABASES = {
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
+}
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+# if not DEBUG:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
