8f83fed6a1ad8625429d887ac0b84d2d3c5db11d
OsaeAddo <immanueloaddo@gmail.com>
2022-07-27 11:16:15 +0000

    update static root

--- mbv/config/settings/prod-settings.py
+++ mbv/config/settings/prod-settings.py
@@ -29,5 +29,5 @@ DATABASES = {
 #     BASE_DIR / 'static'
 # ]
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
