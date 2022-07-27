52abe05df91f96a1d8765ce8356b93daa61a1655
OsaeAddo <immanueloaddo@gmail.com>
2022-07-27 10:42:35 +0000

    fix typo in build.sh file

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 pip install --upgrade pip
 poetry install
 
-python prod_manage.py collectstatic --no-input
+python prod-manage.py collectstatic --no-input
 python prod-manage.py makemigrations 
 python prod-manage.py migrate
 python prod-manage.py showmigrations
