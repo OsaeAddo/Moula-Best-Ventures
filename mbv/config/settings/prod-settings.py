ed37ec9a36e023095478b6e84da2065135a31488
OsaeAddo <immanueloaddo@gmail.com>
2022-07-28 11:23:07 +0000

    add allowed hosts

--- mbv/config/settings/prod-settings.py
+++ mbv/config/settings/prod-settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-pzqirc45gpvlk
 
 DEBUG = 'RENDER' not in os.environ
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["www.moulabestventures.com"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
